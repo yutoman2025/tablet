fc2225633151295d4961fc60b57bfe4c26fdf644
motisukeeva <108178540+motisukeeva@users.noreply.github.com>
2025-12-19 11:27:34 +0900

    運転通告開始

--- TRAIN CREW Mobile Tablet/運転通達受領.Designer.cs
+++ TRAIN CREW Mobile Tablet/運転通達受領.Designer.cs
@@ -28,18 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
+            panel1 = new Panel();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            panel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 20F, FontStyle.Bold);
+            label1.Location = new Point(179, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(179, 37);
+            label1.TabIndex = 1;
+            label1.Text = "運転通告受領";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Yu Gothic UI", 20F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            label2.Location = new Point(34, 72);
+            label2.Name = "label2";
+            label2.Size = new Size(125, 37);
+            label2.TabIndex = 2;
+            label2.Text = "通告内容";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(checkBox2);
+            panel1.Controls.Add(checkBox1);
+            panel1.Location = new Point(12, 92);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(516, 208);
+            panel1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(3, 20);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(83, 19);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(3, 45);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(83, 19);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "checkBox2";
+            checkBox2.UseVisualStyleBackColor = true;
             // 
             // 運転通達受領
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(531, 736);
+            ClientSize = new Size(540, 736);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(panel1);
             Name = "運転通達受領";
             Text = "運転通達受領";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Label label1;
+        private Label label2;
+        private Panel panel1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
     }
 }
